340712bad1aca8a25fc7b672ab027f0b8810a2e8
Calvin Foster <cal.j.foster@gmail.com>
2020-02-02 23:47:25 -0600

    adjustments made

--- AllOutGrind/DataModels/User.cs
+++ AllOutGrind/DataModels/User.cs
@@ -12,5 +12,6 @@ namespace AllOutGrind.DataModels
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string FirebaseUid { get; set; }
     }
 }
