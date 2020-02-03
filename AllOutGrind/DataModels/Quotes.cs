e515cc5459ffcad8061db91fca01637d4ea413a3
Calvin Foster <cal.j.foster@gmail.com>
2020-02-03 05:00:00 -0600

    ready to finish this

---
+++ AllOutGrind/DataModels/Quotes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllOutGrind.DataModels
+{
+    public class Quotes
+    {
+        public Guid Id { get; set; }
+        public string Quote { get; set; }
+        public string ArtistName { get; set; }
+        public string SongName { get; set; }
+    }
+}
