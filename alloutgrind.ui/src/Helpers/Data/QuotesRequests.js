01ac3556b8dc9b8ba4cfbf261eface63ab75626b
Calvin Foster <cal.j.foster@gmail.com>
2020-02-03 21:05:58 -0600

    changes made to the ui

---
+++ alloutgrind.ui/src/Helpers/Data/QuotesRequests.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const baseUrl = 'https://localhost:44361/api/quotes'
+
+const getAllQuotes = () => new Promise ((resovle, reject) => {
+    axios.get(`${baseUrl}`)
+    .then(reult => resovle(result.data))
+    .catch(err => reject(err));
+})
