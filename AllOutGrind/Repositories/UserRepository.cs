a57f98888715ff04f08581cb8e8e94a87b084555
Calvin Foster <cal.j.foster@gmail.com>
2020-02-04 21:33:38 -0600

    able to retrieve email requests

--- AllOutGrind/Repositories/UserRepository.cs
+++ AllOutGrind/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using AllOutGrind.DataModels;
@@ -9,62 +10,52 @@ namespace AllOutGrind.Repositories
 {
     public class UserRepository
     {
-        string _connectionstring = "Server=localhost;Database=AllOutGrind;Trusted_Connection=True;";
-        public bool UserEmailCheck(string newUserEmailCheck)
+        string _connectionString = "Server=localhost;Database=AllOutGrind;Trusted_Connection=True;";
+        public IEnumerable<User> GetAllUsers()
         {
-            using (var db = new SqlConnection( _connectionstring))
+            using (var db = new SqlConnection(_connectionString))
             {
-                var sql = @"SELECT *
-                            FROM [User]";
-                var parameters = new { newUserEmailCheck };
-                var emailAddressComesBack = db.Query<User>(sql, parameters);
-                if (emailAddressComesBack.Count() != 0)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                var users = db.Query<User>(@"SELECT * FROM [User]");
+                return users;
             }
         }
 
-        public bool AddNewUser(AddNewUserDto newUser)
-        {
-            using (var db = new SqlConnection(_connectionstring))
-            {
-                var emailAddressExists = UserEmailCheck(newUser.Email);
-                if (emailAddressExists)
-                {
-                    return false;
-                }
-                var sql = @"
-                            INSERT INTO [User]
-                                (Email,
-                                 FirebaseUid)
-                            OUTPUT INSERTED.Id
-                            VALUES
-                               (@email,
-                                @firebaseuid)";
-                var userId = db.QueryFirst<Guid>(sql, newUser);
-                if (userId != null) return true;
-                else return false;
-            }
-        }
+        //public bool AddNewUser(AddNewUserDto newUser)
+        //{
+        //    using (var db = new SqlConnection(_connectionString))
+        //    {
+        //        var emailAddressExists = UserEmailCheck(newUser.Email);
+        //        if (emailAddressExists)
+        //        {
+        //            return false;
+        //        }
+        //        var sql = @"
+        //                    INSERT INTO [User]
+        //                        (Email,
+        //                         FirebaseUid)
+        //                    OUTPUT INSERTED.Id
+        //                    VALUES
+        //                       (@email,
+        //                        @firebaseuid)";
+        //        var userId = db.QueryFirst<Guid>(sql, newUser);
+        //        if (userId != null) return true;
+        //        else return false;
+        //    }
+        //}
 
         internal bool EditUser(User editedUser)
         {
             throw new NotImplementedException();
         }
 
-        public User GetUserByFirebaseUid(string firebaseUid)
+        public User GetUserByEmail(string Email)
         {
-            using (var db = new SqlConnection(_connectionstring))
+            using (var db = new SqlConnection(_connectionString))
             {
                 var sql = @"SELECT  * 
                             FROM [User]
-                            WHERE [Firebaseuid] = @firebseuid";
-                var parameters = new { firebaseUid };
+                            WHERE [Email] = @Email";
+                var parameters = new { Email };
                 var user = db.QueryFirstOrDefault<User>(sql, parameters);
                 return user;
             }
